c27f58542f98ca40e414d3ee79431d2febb111f4
papagvas <papagvasexy@gmail.com>
2026-01-13 14:04:17 +0300

    Initial commit

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import ModernCompilerLean
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ ModernCompilerLean.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `ModernCompilerLean` library.
+-- Import modules here that should be built as part of the library.
+import ModernCompilerLean.Basic
